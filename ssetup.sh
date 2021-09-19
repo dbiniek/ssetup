910ac826111768d961d2998eb86a7f6190b7eaa0
root <root@bikini.davehasroot.com>
2021-09-19 12:20:25 -0600

    first commit

---
+++ ssetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##Setup script for new domains added to the server
+
+##create directories
+
+##set skeleton template and perms
+
+##apache conf/nginx conf
+
+##named conf
+
+##certbot
